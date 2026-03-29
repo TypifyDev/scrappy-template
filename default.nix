838711cc0944031fd5310eeaa358f73a48f75d09
lazyLambda <galen.sprout@gmail.com>
2026-03-29 14:53:32 -0400

    Rename org to TypifyDev

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , modern-uri, network-uri, parsec, parser-combinators, text
 , transformers, pkgs
 , scrappy-core ? pkgs.haskellPackages.callPackage (pkgs.fetchFromGitHub {
-    owner = "Ace-Interview-Prep";
+    owner = "TypifyDev";
     repo = "scrappy-core";
     rev = "913670f2f83cabb2b56302e17604ec488e89da7b";
     sha256 = "sha256-qA6+5Lxsw5q0DWYNx0VnvCmYo2f2/oKvDPVH0ZNgfXc=";
@@ -16,7 +16,7 @@ mkDerivation {
     aeson base bytestring containers lens modern-uri network-uri parsec
     parser-combinators text transformers scrappy-core
   ];
-  homepage = "https://github.com/Ace-Interview-Prep/scrappy";
+  homepage = "https://github.com/TypifyDev/scrappy";
   description = "html pattern matching library and high-level interface concurrent requests lib for webscraping";
   license = lib.licenses.bsd3;
 }
